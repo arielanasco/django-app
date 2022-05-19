69e0fc3496576d0dbfe4def6fd90dec48d6fdcaf
arielanasco <ariel@linkmind.co.jp>
2022-05-19 18:17:39 +0800

    Add download CSV function

--- learn/admin.py
+++ learn/admin.py
@@ -70,10 +70,8 @@ from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 class UserAdmin(BaseUserAdmin):
     def get_queryset(self, request):
         qs = super(UserAdmin, self).get_queryset(request)
-        if request.user.is_superuser:
-            return qs
         return qs.filter(is_superuser=False)    
-        
+
 # class UserAdmin(admin.ModelAdmin):
 #     def get_queryset(self, request):
 #         qs = super(UserAdmin, self).get_queryset(request)
